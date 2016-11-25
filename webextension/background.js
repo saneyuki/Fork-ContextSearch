ff02e75eb200d8e0c254fa425c2fee01dd6221e8
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-11-26 00:46:42 +0900

    Implement the logic opening a new tab  for background.js

--- webextension/background.js
+++ webextension/background.js
@@ -6,3 +6,63 @@
 
 /*global browser: false */
 /* eslint-disable no-implicit-globals */
+
+const port = browser.runtime.connect({});
+port.onMessage.addListener((msg) => {
+  const { type, id, value } = msg;
+  switch (type) {
+    case "contextsearch-open-tab": {
+      const { url, where } = value;
+      const response = {
+        id,
+        type: "context-search-open-tab-result",
+        value: null,
+      };
+      const creating = createTab(url, where);
+      creating.then((tabId) => {
+        response.value = {
+          ok: true,
+          tabId: tabId,
+          error: null,
+        };
+        port.postMessage(response);
+      }, (e) => {
+        response.value = {
+          ok: false,
+          tabId: null,
+          error: e.message,
+        };
+        port.postMessage(response);
+      });
+      break;
+    }
+  }
+});
+
+/**
+ *  @param  {string}  url
+ *  @param  {string}  where
+ *  @returns  {number}
+ *    `tabs.Tab.id`. integer.
+ */
+async function createTab(url, where) {
+  const option = {
+    active: false,
+    url,
+    windowId: null,
+  };
+
+  switch (where) {
+    case "tab":
+      option.active = true;
+      break;
+    case "tabshifted":
+      break;
+    default:
+      throw new RangeError("unexpeced where type");
+  }
+
+  const newTab = await browser.tabs.create(option);
+  return newTab.id;
+}
+
